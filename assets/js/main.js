5687076176aab1bc3cd79a6295456b845b3d6b9f
Valdir Amaral <valdir.amaral58@gmail.com>
2025-04-12 22:03:28 -0300

    feat: add sections and addToCart method

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,41 @@ function search(ev) {
     
     alert(`Você pesquisou por ${searchInput.value}`);
 }
+
+const productButtons = document.querySelectorAll('.product-card button.button');
+console.log(productButtons);
+
+productButtons.forEach(b => b.addEventListener('click', addToCart));
+
+function addToCart(ev) {
+    console.log(ev);
+    let btnSelected = ev.currentTarget;
+    btnSelected.innerHTML = '<span class="loader-product"></span>';
+    btnSelected.setAttribute('disabled', true);
+
+    setTimeout(() => {
+        const toastHTML = `
+            <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <strong class="me-auto">Bootstrap</strong>
+                <small>11 mins ago</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                Hello, world! This is a toast message.
+            </div>
+            </div>
+        `;
+
+        // Cria um elemento temporário para inserir o HTML
+        const temp = document.createElement('div');
+        temp.innerHTML = toastHTML;
+
+        // Adiciona o toast ao body
+        document.body.appendChild(temp.firstElementChild);
+
+        btnSelected.setAttribute('disabled',false);
+        btnSelected.innerHTML = "Comprar";
+       
+    }, 800)
+}
